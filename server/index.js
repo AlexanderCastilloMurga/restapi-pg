109fbdc523fe420a380434636caef13f747b0619
AlexanderCastilloMurga <alex.strange.1920@gmail.com>
2024-03-08 18:40:03 -0600

    k

---
+++ server/index.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+
+const server = require('./server/index.js')
+
+const PORT = process.env.PORT || 3300
+
+console.log(PORT)
+
+server.listen(PORT, () => console.log ('Server is live at Localhost:'+ PORT));
